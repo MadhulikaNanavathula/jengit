b690bf58ac6426a4b3d25b28e63a3a935b5fd499
Nanavathula Madhulika <nanavathulamadhulika@gmail.com>
2025-09-18 16:01:17 +0530

    Update Hello.java

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello from Pipeline CI/CD!");
     System.out.println("Hi");
+    System.out.println("Hi");
   }
 }
